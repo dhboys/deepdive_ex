d105ee5abceea3acfe36aa3be975103da02371bc
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-06-20 23:07:24 +0900

    failure_ratio 70 point result

--- cote/programmers/lv_1/failure_ratio.test.js
+++ cote/programmers/lv_1/failure_ratio.test.js
@@ -7,12 +7,26 @@ test("실패율 구하기", () => {
   let leftUserNum = stages.length;
   let failureRatioObj = {};
   // 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return
-  for (let i = 1; i <= N; i++) {
+  for (let i = 1; i <= N + 1; i++) {
     const stuckUserNum = stages.filter((v) => i === v).length;
+    failureRatioObj[i] = stuckUserNum / leftUserNum;
     leftUserNum -= stuckUserNum;
-    failureRatioObj[i] = failureRatio;
   }
 
-  console.log("failureRatioArray", failureRatioArray);
-  console.log("failureRatioObj", failureRatioObj);
+  const valueArray = Object.values(failureRatioObj).sort((a, b) => b - a);
+  let returnArray = [];
+
+  for (let i = 0; i < valueArray.length; i++) {
+    for (let j = 0; j < valueArray.length; j++) {
+      if (valueArray[i] === failureRatioObj[j]) {
+        returnArray.push(parseInt(j));
+      }
+    }
+  }
+
+  const setReturnArray = [...new Set(returnArray)];
+
+  // console.log("failureRatioObj", failureRatioObj);
+  // console.log("valueArray", valueArray);
+  console.log("setReturnArray", setReturnArray);
 });
