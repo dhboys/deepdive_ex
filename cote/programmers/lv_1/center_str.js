ceb5717b95c657d02b58d6fe2bf2d0e3d1f0d6e4
dhboys <dhboys@icloud.com>
2022-06-29 09:45:26 +0900

    2022-06-29 가운데 글자 가져오기, 최소 직사각형 코드 간소화

--- cote/programmers/lv_1/center_str.js
+++ cote/programmers/lv_1/center_str.js
@@ -1,13 +1,11 @@
 test("가운데 글자 가져오기", () => {
   // 홀수 값 구할 때 floor 함수 사용하기
-  const s = "abde";
+  const s = "abcde";
   let result = "";
   const strLength = s.length;
   if (strLength % 2 !== 0) {
-    result = s.slice(strLength / 2, strLength / 2 + 1);
+    return s[Math.floor(strLength / 2)];
   } else {
-    result = s.slice(strLength / 2 - 1, strLength / 2 + 1);
+    return s.slice(strLength / 2 - 1, strLength / 2 + 1);
   }
-
-  // console.log("result", result);
 });
--- cote/programmers/lv_1/min_rectangle.js
+++ cote/programmers/lv_1/min_rectangle.js
@@ -8,7 +8,11 @@ test("최소 직사각형", () => {
   // 한쪽에 큰 수 몰아 넣고 -> sort
   // 다른 한 쪽에 그 외에 수를 몰아 넣고
   // 각각 max 값 구해서 곱 -> Math.max()
-  const sortedSizes = sizes.sort((a, b) => a - b);
-  const xMax = 0;
-  const yMax = 0;
+  const sortedSizes = sizes.map((v) => v.sort((a, b) => a - b));
+  let xMax = 0;
+  let yMax = 0;
+  xMax = Math.max(...sortedSizes.map((v) => v[0]));
+  yMax = Math.max(...sortedSizes.map((v) => v[1]));
+  const result = xMax * yMax;
+  return result;
 });
