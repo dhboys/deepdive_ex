e3ba3951783f273db76c9d2061a0b66d240751d7
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-07-01 23:22:15 +0900

    2022-07-01 신고 결과 받기 시간 8/24 시간초과.. 다시 풀기

--- cote/programmers/lv_1/declaration_result.js
+++ cote/programmers/lv_1/declaration_result.js
@@ -9,12 +9,20 @@ test("신고 결과 받기", () => {
   ];
   const k = 2;
 
-  const reporterArray = report.map((v) => v.split(" ")[0]);
-  console.log("reporterArray", reporterArray);
-  const bad = report.map((v) => v.split(" ")[1]);
-  console.log("bad", bad);
-
+  let reporter = [];
+  let result = [];
+  const reporterArray = [...new Set(report)].map((v) => v.split(" "));
   for (let i = 0; i < id_list.length; i++) {
-    bad.filter((v) => v === id_list[i]).length >= k;
+    for (let j = 0; j < reporterArray.length; j++) {
+      if (reporterArray.filter((v) => v[1] === id_list[i]).length >= k) {
+        reporterArray[j][1] === id_list[i]
+          ? reporter.push(reporterArray[j][0])
+          : null;
+      }
+    }
   }
+  for (let i = 0; i < id_list.length; i++) {
+    result.push(reporter.filter((v) => v === id_list[i]).length);
+  }
+  console.log("result", result);
 });
