35a1dbb2cdb2d0b11c9a96d138b1e5fe945739e6
dhboys <dhboys@icloud.com>
2022-07-04 09:16:05 +0900

    2022-07-04 신고 결과 받기 다른 사람 풀이 참조

--- cote/programmers/lv_1/declaration_result.js
+++ cote/programmers/lv_1/declaration_result.js
@@ -25,4 +25,23 @@ test("신고 결과 받기", () => {
     result.push(reporter.filter((v) => v === id_list[i]).length);
   }
   console.log("result", result);
+
+  // map을 만들어 사용할 때 key, value를 지정하고 싶다면 new Map()으로 map을 만들고 set 함수를 사용한다.
+
+  // 다른 사람의 풀이 참고
+  let reports = [...new Set(report)].map((a) => {
+    return a.split(" ");
+  });
+  let counts = new Map();
+  for (const bad of reports) {
+    counts.set(bad[1], counts.get(bad[1]) + 1 || 1);
+  }
+  let good = new Map();
+  for (const report of reports) {
+    if (counts.get(report[1]) >= k) {
+      good.set(report[0], good.get(report[0]) + 1 || 1);
+    }
+  }
+  let answer = id_list.map((a) => good.get(a) || 0);
+  return answer;
 });
