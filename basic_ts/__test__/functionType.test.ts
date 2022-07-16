be2dc0cb7624640fc40220948ece29b1779bf3d1
dhboys <dhboys@icloud.com>
2022-07-16 14:22:39 +0900

    :sparkles:

---
+++ basic_ts/__test__/functionType.test.ts
@@ -0,0 +1,11 @@
+test("함수 타입", () => {
+  interface login {
+    (username: string, password: string): boolean;
+  }
+
+  let loginUser: login;
+  loginUser = function (id: string, pw: string) {
+    console.log("로그인 했습니다.");
+    return true;
+  };
+});
