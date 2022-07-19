1f92fb5668d652170acbd1cc45ceb724586d5289
dhboys <dhboys@icloud.com>
2022-07-19 09:29:39 +0900

    2022-07-19 localeCompare와 sort 함수 공부 필요

--- cote/programmers/lv_1/str_sort_freely.test.js
+++ cote/programmers/lv_1/str_sort_freely.test.js
@@ -1,8 +1,9 @@
 test("문자열 내 마음대로 정렬하기", () => {
   const strings = ["sun", "bed", "car"];
   const n = 1;
-  const wordMapList = strings.map((word) => [word[n], word]);
-  wordMapList
-    .sort((a, b) => (a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0))
-    .map((v) => v[1]);
+  return strings.sort((s1, s2) =>
+    s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n])
+  );
+
+  // 내부 char가 같으면 전체 string을 비교, 내부 char 가 다르면 내부 char 비교
 });
