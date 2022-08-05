aef20e6461dcf7943eaae7a27625205826ded637
dhboys <dhboys@icloud.com>
2022-08-05 13:09:34 +0900

    2022-08-05 testcase 1, 16 만 통과 (Leave는 uid만 받는다..)

--- cote/programmers/lv_2/open_chat.test.js
+++ cote/programmers/lv_2/open_chat.test.js
@@ -1,45 +1,59 @@
 test("오픈 채팅방", () => {
-    const record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"];
-    let result = []
-    let userIdMap = new Map();
+  const record = [
+    "Enter uid1234 Prodo",
+    "Enter uid4567 Muzi",
+    "Change uid4567",
+    "Enter uid7890 Apeach",
+    "Leave uid4567",
+    "Enter uid4567 Ryan",
+    "Change uid4567 Prodo",
+  ];
+  let result = [];
+  let userIdMap = new Map();
 
-    const returnAction = (key) => {
-        switch(key) {
-            case 'Enter':
-                return "님이 들어왔습니다."
-                break;
-            case 'Leave':
-                return "님이 나갔습니다."
-                break;
-            default:
-            break;
-        }
+  const returnAction = (key) => {
+    switch (key) {
+      case "Enter":
+        return "님이 들어왔습니다.";
+        break;
+      case "Leave":
+        return "님이 나갔습니다.";
+        break;
+      case "Change":
+        return "닉네임 변경";
+        break;
+      default:
+        break;
     }
+  };
 
-    record.map((action) => {
-        if(action.split(' ')[0] === 'Enter') {
-            userIdMap[action.split(' ')[1]] = action.split(' ')[2]
-            result.push([action.split(' ')[1], 'Enter']);
-        } else if (action.split(' ')[0] === 'Leave') {
-            userIdMap[action.split(' ')[1]] = action.split(' ')[2]
-            result.push([action.split(' ')[1], 'Leave']);
-        } else if ((action.split(' ')[0] === 'Change')) {
-            userIdMap[action.split(' ')[1]] = action.split(' ')[2]
-            result.push([action.split(' ')[1], 'Change']);
-        }
-    })
-    
-    result.map((action, idx) => { 
-        action[0] = userIdMap[action[0]]
-        if (returnAction(action[1]) === undefined) {
-            result.splice(idx, 1)
-        } else {
-            action[1] = returnAction(action[1])
-        } 
-        return action
-    })
-    return result.map((action) => { 
-            return action[0] + action[1]
-    })
+  record.map((action) => {
+    if (action.split(" ")[0] === "Enter") {
+      userIdMap[action.split(" ")[1]] = action.split(" ")[2];
+      result.push([action.split(" ")[1], "Enter"]);
+    } else if (action.split(" ")[0] === "Leave") {
+      userIdMap[action.split(" ")[1]] = action.split(" ")[2];
+      result.push([action.split(" ")[1], "Leave"]);
+    } else if (action.split(" ")[0] === "Change") {
+      userIdMap[action.split(" ")[1]] = action.split(" ")[2];
+      result.push([action.split(" ")[1], "Change"]);
+    }
   });
-  
+  //   console.log("before result", result);
+
+  result.map((action, idx) => {
+    action[0] = userIdMap[action[0]];
+
+    action[1] = returnAction(action[1]);
+
+    return action;
+  });
+  //   console.log("after result", result);
+  return result
+    .filter((change) => change[1] !== "닉네임 변경")
+    .map((action) => {
+      return action[0] + action[1];
+    });
+
+  // Leave는 uid만 받는다는 사실 상기 필요
+});
