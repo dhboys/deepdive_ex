d3fe9cf7077ac48377f0a3c94017c40bb807bddd
dhboys <dhboys@icloud.com>
2022-09-13 09:40:50 +0900

    2022-09-13 life_boat cote in progress

--- cote/programmers/lv_2/life_boat.test.js
+++ cote/programmers/lv_2/life_boat.test.js
@@ -1,5 +1,27 @@
 test("구명 보트", () => {
-    const people = [70, 50, 80, 50]
-    const limit = 100
-  });
-  
+  const people = [70, 50, 80, 50];
+  const limit = 100;
+  let sortedPeople = people.sort((a, b) => a - b);
+  let count = 0;
+
+  for (var i = 0; i < sortedPeople.length; i++) {
+    for (var j = 0; j < sortedPeople.length; j++) {
+      if (sortedPeople[i] + sortedPeople[j] === 100) {
+        sortedPeople[i] = 1000;
+        sortedPeople[j] = 1000;
+        count++;
+        continue;
+      } else if (sortedPeople[i] + sortedPeople[j] < 100) {
+        sortedPeople[i] = 1000;
+        sortedPeople[j] = 1000;
+        count++;
+        continue;
+      } else {
+        count++;
+        continue;
+      }
+    }
+  }
+  console.log("sortedPeople", sortedPeople);
+  console.log("count", count);
+});
