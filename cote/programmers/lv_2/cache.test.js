215e038db3173c5c13bee01c6010d2ac7614695b
dhboys <dhboys@icloud.com>
2022-09-23 09:57:26 +0900

    2022-09-23 cache (testcase 11, 15, 16, 18, 19 error)

---
+++ cote/programmers/lv_2/cache.test.js
@@ -0,0 +1,37 @@
+test("카펫", () => {
+  const cacheSize = 0;
+  const cities = [
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+  ];
+  const cacheHit = 1;
+  const cacheMiss = 5;
+  let runTime = 0;
+  let stackList = [];
+  cities.map((city) => {
+    const lowerCaseCity = city.toLowerCase();
+    if (!stackList.includes(lowerCaseCity)) {
+      stackList.unshift(lowerCaseCity);
+      runTime += 5;
+      if (cacheSize < stackList.length) {
+        stackList.pop();
+      }
+    } else if (stackList.length < cacheSize) {
+      stackList = stackList.filter((city) => city !== lowerCaseCity);
+      stackList.push(lowerCaseCity);
+      runTime++;
+    } else {
+      stackList = stackList.filter((city) => city !== lowerCaseCity);
+      stackList.push(lowerCaseCity);
+      runTime++;
+    }
+  });
+  console.log("runTime", runTime);
+});
