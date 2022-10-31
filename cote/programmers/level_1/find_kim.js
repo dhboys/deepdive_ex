1e6e3f675ec548f44426929fea5030d715cb1726
dhboys <dhboys@icloud.com>
2022-11-01 08:57:42 +0900

    2022-11-01 level1 1개 pass && 1개 in progress

---
+++ cote/programmers/level_1/find_kim.js
@@ -0,0 +1,4 @@
+test("서울에서 김서방 찾기", () => {
+  const seoul = ["Jane", "Kim"];
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+});
---
+++ cote/programmers/level_1/hide_mobile_number.test.js
@@ -0,0 +1,4 @@
+test("핸드폰 번호 가리기", () => {
+  const phone_number = "01033334444";
+  console.log(phone_number.split(""));
+});
